eaf88e3e85b51ef76710e55680a4bdfa84a85200
root <graybismuthmaster@hotmail.com>
2023-02-13 12:51:21 -0500

    build

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import app from './app'
 require('dotenv').config({path: 'variables.env'})
 import dbConexion from './config/db'
 
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
-    console.log('Esta en desarrollo');
-    const mongoEnvDev = process.env.DB_MONGO_DEV;
-    dbConexion(mongoEnvDev);    
-    //const server = http.createServer(app);
-    app.listen({port:process.env.DEV_PORT||5001},()=>{
-        console.log(`Servidor Levantado en el puerto ${process.env.DEV_PORT}`);
-    }); 
-}else{
     console.log("production");
     const mongoEnvPro = process.env.DB_MONGO_ATLAS_PRO;
     dbConexion(mongoEnvPro) 
@@ -19,5 +9,3 @@ if(process.env.NODE_ENV !== 'production'){
     app.listen({port:process.env.PROD_PORT||5001},()=>{
         console.log(`Servidor Levantado en el puerto ${process.env.PROD_PORT}`);
     }); 
-}
-
